42d551d6f8cf80ad0979fa4928edd9dc164e0ae2
José Carlos Martins <naoedojose.nave@gmail.com>
2015-12-11 08:39:02 -0200

    Start of the project
    Only that :dash:

---
+++ Assets/Resources/Scripts/NetworkManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	private const string typeName = "UniqueGameNaame";
+	private const string gameName  = "RoomName";
+	public int maxPlayers = 2;
+	public int port = 3128;
+
+	void Start(){
+		StartServer ();
+	
+	}
+
+	private void StartServer(){
+
+		Network.InitializeServer (maxPlayers, port, !Network.HavePublicAddress ());
+		MasterServer.RegisterHost (typeName, gameName);
+	}
+	void OnServerInitialized(){
+
+		Debug.Log("Server Initialized");
+	}
+}
